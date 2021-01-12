0acf49e613bd4736c52d0c087c5d9233c81f0ca6
Gipsyzgm <458586540@qq.com>
2021-01-12 18:54:17 +0800

    热更环境

---
+++ HotFix/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HotFix
+{
+    public class Main
+    {
+
+        public static void Start()
+        {
+           Debug.Log("=====进入热更主程序,启动游戏!!!!=====");
+             
+        }
+
+    }
+}
