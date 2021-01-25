d7cde8f82754ca12328317c9e27a7e7a0be2df72
Gipsyzgm <458586540@qq.com>
2021-01-25 19:50:41 +0800

    使用UnityAsyncRoutines代替Unitask

---
+++ MyPro/Assets/MyCore/Manager/UnityAsyncRoutines/CTaskExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class CTaskExtension
+{
+    public static CTaskHandle Run(this CTask task)
+    {
+        return Mgr.Task.Manager.Run(task);
+    }
+}
---
+++ MyPro/Assets/MyCore/Manager/UnityAsyncRoutines/CTaskMgr.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+//https://github.com/tomblind/unity-async-routines
+public class CTaskMgr : BaseMgr<CTaskMgr>
+{
+    public RoutineManager Manager { get { return routineManager; } }
+
+    private RoutineManager routineManager = new RoutineManager();
+
+    public CTaskMgr()
+    {
+        CTask.TracingEnabled = false;
+    }
+    public virtual void Update()
+    {
+        routineManager.Update();
+    }
+
+    public virtual void LateUpdate()
+    {
+        routineManager.Flush();
+    }
+
+    public void OnDestroy()
+    {
+        routineManager.StopAll();
+    }
+
+    /// <summary> Manages and runs a routine. </summary>
+    public CTaskHandle Run(CTask routine, Action<Exception> onStop = null)
+    {
+        return routineManager.Run(routine, onStop);
+    }
+
+
+    /// <summary> Stops all managed routines. </summary>
+    public void StopAll()
+    {
+        routineManager.StopAll();
+    }
+}
