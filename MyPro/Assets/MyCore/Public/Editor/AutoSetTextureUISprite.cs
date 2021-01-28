66c47eced7c8ff976729e765a7c6e36cb6213946
Gipsyzgm <458586540@qq.com>
2021-01-28 19:03:51 +0800

    新增粒子效果插件，支持UI粒子，导入图片直接转换sprite

---
+++ MyPro/Assets/MyCore/Public/Editor/AutoSetTextureUISprite.cs
@@ -0,0 +1,13 @@
+﻿/************************** * 文件名:AutoSetTextureUISprite.cs; * 文件描述:导入图片资源到Unity时，自动修改为UI 2D Sprite***************************/
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class AutoSetTextureUISprite : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        //自动设置类型;
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+    }
+}
