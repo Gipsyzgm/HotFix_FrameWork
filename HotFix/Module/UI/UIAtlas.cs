c25b162a1cbc8e9b0061ae329211927acf111eb0
Gipsyzgm <458586540@qq.com>
2021-03-12 11:11:54 +0800

    添加自动生成的图集路径

---
+++ HotFix/Module/UI/UIAtlas.cs
@@ -0,0 +1,10 @@
+﻿//每次都会重新生成的脚本，不要删，覆盖就行了
+namespace HotFix
+{
+    public class UIAtlas
+    {
+        public const string PublicBK = "PublicBK";
+        public const string PublicButton = "PublicButton";
+        public const string PublicSymbol = "PublicSymbol";
+    }
+}
