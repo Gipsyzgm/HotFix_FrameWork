8a0411f07d1007ba287ef5d050bf9421fa89081e
Gipsyzgm <458586540@qq.com>
2021-03-15 18:10:57 +0800

    自动添加引用调整

--- MyPro/Assets/MyCore/UIExtend/Editor/AutoSetUISpriteAtlas.cs
+++ MyPro/Assets/MyCore/UIExtend/Editor/AutoSetUISpriteAtlas.cs
@@ -7,6 +7,8 @@ using System.IO;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.U2D;
+using UnityEditor.Experimental.SceneManagement;
+using System.Threading.Tasks;
 /// <summary>
 /// 自动设置UI引用的SpriteAtlsas
 /// </summary>
@@ -14,25 +16,28 @@ public class AutoSetUISpriteAtlas : UnityEditor.AssetModificationProcessor
 {
     //UI目录等同于AppSetting.AllUIPrefabs
     private static string UI_DIR = AppSetting.AllUIPrefabs;
-
     #region Apply
     [InitializeOnLoadMethod]
     static void StartInitializeOnLoadMethod()
-    {
+    { 
+        // Prefab被保存之前回调
+        PrefabStage.prefabSaving += SaveUIPrefabInHierachy;
+
         //注册Apply时的回调
         PrefabUtility.prefabInstanceUpdated = delegate (GameObject instance)
         {
             if (instance)
-                SaveUIPrefab(instance);
+                SaveUIPrefabInProgect(instance);
         };
+
     }
     /// <summary>
     /// 保存UI预制
     /// 自动添加引用图集依赖
     /// </summary>
     /// <param name="instance"></param>
-    public static void SaveUIPrefab(GameObject instance)
-    {
+    public static void SaveUIPrefabInProgect(GameObject instance)
+    {     
         string prefabPath = AssetDatabase.GetAssetPath(PrefabUtility.GetCorrespondingObjectFromSource(instance));
         if (!IsUIPrefab(prefabPath))
             return;
@@ -75,7 +80,53 @@ public class AutoSetUISpriteAtlas : UnityEditor.AssetModificationProcessor
             if (compAtlas != null)
                 Component.DestroyImmediate(compAtlas, true);
         }
-        PrefabUtility.RevertObjectOverride(instance,InteractionMode.AutomatedAction);
+        PrefabUtility.RevertObjectOverride(instance, InteractionMode.AutomatedAction);
+        AssetDatabase.Refresh();
+    }
+
+    public static void SaveUIPrefabInHierachy(GameObject instance)
+    {
+        if (instance.GetComponent<UIOutlet>()==null)
+            return;
+        Image[] imgs = instance.GetComponentsInChildren<Image>(true);
+        Dictionary<string, SpriteAtlas> saDict = new Dictionary<string, SpriteAtlas>();
+        List<SpriteAtlas> saList = new List<SpriteAtlas>();
+        string imgPath;
+        string spriteAtlasPath;
+        SpriteAtlas sa;
+        foreach (Image img in imgs)
+        {
+            imgPath = AssetDatabase.GetAssetPath(img.sprite);
+            if (imgPath.IndexOf("/UIAtlas/") == -1) continue;
+            imgPath = imgPath.Substring(0, imgPath.LastIndexOf("/"));
+            spriteAtlasPath = imgPath.Replace("/ArtRes/UIAtlas/", "/AddressableRes/UIAtlas/") + ".spriteatlas";
+            if (!saDict.TryGetValue(spriteAtlasPath, out sa))
+            {
+                sa = AssetDatabase.LoadAssetAtPath<SpriteAtlas>(spriteAtlasPath);
+                if (sa != null)
+                {
+                    saDict.Add(spriteAtlasPath, sa);
+                    saList.Add(sa);
+                }
+                else
+                {
+                    Debug.LogWarning("SpriteAtlas未找到:" + spriteAtlasPath);
+                }
+            }
+        }
+        SpriteAtlasList compAtlas = instance.GetComponent<SpriteAtlasList>();
+        if (saList.Count > 0)
+        {
+            if (compAtlas == null)
+                compAtlas = instance.AddComponent<SpriteAtlasList>();
+            compAtlas.AtlasList = saList.ToArray();
+        }
+        else
+        {
+            if (compAtlas != null)
+                Component.DestroyImmediate(compAtlas, true);
+        }
+        AssetDatabase.Refresh();
     }
     #endregion
 
@@ -90,5 +141,4 @@ public class AutoSetUISpriteAtlas : UnityEditor.AssetModificationProcessor
             return true;
         return false;
     }
-
 }
