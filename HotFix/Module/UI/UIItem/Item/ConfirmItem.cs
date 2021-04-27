81a938f5a2b10572a157d9b168007da5309fa02a
Gipsyzgm <458586540@qq.com>
2021-04-27 16:57:54 +0800

    通用页面Confirm

---
+++ HotFix/Module/UI/UIItem/Item/ConfirmItem.cs
@@ -0,0 +1,110 @@
+﻿using HotFix.Module.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+namespace HotFix
+{
+    public partial class ConfirmItem : BaseItem
+    {
+
+        private Action ConfirmCallBack;
+        private Action CancelCallBack;
+
+        /// <summary>添加按钮事件</summary>
+        public override void Init()
+        {
+             //当前对象点击事件需添加Button组件
+            CancelButton.onClick.AddListener(CancelButton_Click);   //
+            ConfirmButton.onClick.AddListener(ConfirmButton_Click);   //
+
+        }
+        /// <summary>刷新Item</summary>
+        public override void Refresh()
+        {
+
+
+           
+        }
+
+        public void ShowCustomConfirm(string content, string title = null,Action ConfirmCB = null, Action CancelCB = null)
+        {
+          
+            //至少一个确认
+            if (ConfirmCB == null)
+            {
+                ConfirmCallBack = null;
+            }
+            else 
+            {
+                ConfirmCallBack = ConfirmCB;
+            }
+            //没有取消事件的话就不显示按钮
+            if (CancelCB == null)
+            {
+                CancelCallBack = null;
+                CancelButton.gameObject.SetActive(false);
+            }
+            else
+            {
+                CancelCallBack = CancelCB;
+                CancelButton.gameObject.SetActive(true);                      
+            }
+            if (title == null)
+            {
+                TitleText.text = "提示";
+            }
+            else 
+            {
+                TitleText.text = title;
+            }
+            ContentText.text = content;
+            CurObj.SetActive(true);
+            //后出现的放后面
+            CurObj.transform.SetAsLastSibling();
+            ShowUIAnim(CurObj, UIAnim.ScaleIn);
+        }
+
+        public void SetMask(bool isShow) 
+        {
+            if (isShow)
+            {
+                Mask.SetActive(true);
+            }
+            else 
+            {
+                Mask.SetActive(false);
+            }
+        }
+
+
+        /// <summary>当前对象点击事件</summary>
+        void self_Click()
+        {
+
+
+        }
+        /// <summary></summary>
+        void CancelButton_Click()
+        {
+            CancelCallBack?.Invoke();
+            ShowUIAnim(CurObj, UIAnim.ScaleOut);
+            Confirm.CacheConfirmsList.Enqueue(this);
+        }
+        /// <summary></summary>
+        void ConfirmButton_Click()
+        {
+            ConfirmCallBack?.Invoke();
+            ShowUIAnim(CurObj, UIAnim.ScaleOut);
+            Confirm.CacheConfirmsList.Enqueue(this);
+        }
+
+        /// <summary>释放Item引用</summary>
+        public override void Dispose()
+        {
+
+        }
+    }
+}
+
---
+++ HotFix/Module/UI/UIItem/View/ConfirmItemView.cs
@@ -0,0 +1,29 @@
+﻿//工具生成不要修改
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace HotFix
+{
+    public partial class ConfirmItem : BaseItem
+    {
+        private GameObject Mask;
+        private Text TitleText;
+        private Text ContentText;
+        private Button CancelButton;
+        private Button ConfirmButton;
+ 
+        /// <summary>初始化UI控件</summary>
+        public override void InitComponent()
+        {
+            Mask = Get("Mask");
+            TitleText = Get<Text>("TitleText");
+            ContentText = Get<Text>("ContentText");
+            CancelButton = Get<Button>("CancelButton");
+            ConfirmButton = Get<Button>("ConfirmButton");
+
+        }
+    }
+}
+
