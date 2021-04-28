1b9a0079a04e13fdb8d352546afb85a61d3b449e
Gipsyzgm <458586540@qq.com>
2021-04-28 22:44:58 +0800

    SettingUI微调

--- HotFix/Module/UI/UIPanel/Panel/SettingUI.cs
+++ HotFix/Module/UI/UIPanel/Panel/SettingUI.cs
@@ -25,16 +25,64 @@ namespace HotFix
                 Debug.Log("开启音效：" + b);
                 HotMgr.Sound.isPlayEffect = b;
             });
+            ZH_CN.onValueChanged.AddListener((b) =>
+            {               
+                HotMgr.Lang.LangType = ELangType.ZH_CN;
+
+            });
+            ZH_TW.onValueChanged.AddListener((b) =>
+            {
+                HotMgr.Lang.LangType = ELangType.ZH_TW;
+
+            });
+            ZH_CN.onValueChanged.AddListener((b) =>
+            {
+                HotMgr.Lang.LangType = ELangType.ZH_CN;
+
+            });
+            JA.onValueChanged.AddListener((b) =>
+            {
+                HotMgr.Lang.LangType = ELangType.JA;
+
+            });
+            KO.onValueChanged.AddListener((b) =>
+            {
+                HotMgr.Lang.LangType = ELangType.KO;
+
+            });
 
             CloseBtn.AddClick(() =>
             {
                 HideSelf(UIAnim.FadeOut);
             });
+
+          
         }
+
 
          /// <summary>刷新</summary>
         public override void Refresh()
         {
+            switch (HotMgr.Lang.LangType)
+            {
+                case ELangType.ZH_CN:
+                    ZH_CN.isOn = true;
+                    break;
+                case ELangType.ZH_TW:
+                    ZH_TW.isOn = true;
+                    break;
+                case ELangType.EN:
+                    EN.isOn = true;
+                    break;
+                case ELangType.JA:
+                    JA.isOn = true;
+                    break;
+                case ELangType.KO:
+                    KO.isOn = true;
+                    break;
+                default:
+                    break;
+            }
         }
 
 
--- HotFix/Module/UI/UIPanel/View/SettingUIView.cs
+++ HotFix/Module/UI/UIPanel/View/SettingUIView.cs
@@ -11,6 +11,11 @@ namespace HotFix
         private Toggle Music;
         private Toggle Sound;
         private Button CloseBtn;
+        private Toggle ZH_CN;
+        private Toggle ZH_TW;
+        private Toggle EN;
+        private Toggle JA;
+        private Toggle KO;
  
         /// <summary>初始化UI控件</summary>
         public override void InitComponent()
@@ -18,6 +23,11 @@ namespace HotFix
             Music = Get<Toggle>("Music");
             Sound = Get<Toggle>("Sound");
             CloseBtn = Get<Button>("CloseBtn");
+            ZH_CN = Get<Toggle>("ZH_CN");
+            ZH_TW = Get<Toggle>("ZH_TW");
+            EN = Get<Toggle>("EN");
+            JA = Get<Toggle>("JA");
+            KO = Get<Toggle>("KO");
 
         }
     }
