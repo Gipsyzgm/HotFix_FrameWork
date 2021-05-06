eb4ebec1050780b22f78349dc8c850d86e4ca1e3
Gipsyzgm <458586540@qq.com>
2021-05-06 18:14:47 +0800

    新增loginui

---
+++ HotFix/Module/UI/UIPanel/Panel/LoginUI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+namespace HotFix
+{
+    public partial class LoginUI : BaseUI
+    {      
+        /// <summary>添加按钮事件</summary>
+        public override void Init(params object[] _args)
+        { 
+            args = _args;
+            LoginButton.onClick.AddListener(LoginButton_Click);   //
+            SdkLogin.onClick.AddListener(SdkLogin_Click);   //
+            ChangServerBtn.onClick.AddListener(ChangServerBtn_Click);   //
+            StartGameButton.onClick.AddListener(StartGameButton_Click);   //
+            LoginOutButton.onClick.AddListener(LoginOutButton_Click);   //
+
+        }
+
+         /// <summary>刷新</summary>
+        public override void Refresh()
+        {
+        }
+
+        /// <summary></summary>
+        void LoginButton_Click()
+        {
+        }
+        /// <summary></summary>
+        void SdkLogin_Click()
+        {
+        }
+        /// <summary></summary>
+        void ChangServerBtn_Click()
+        {
+        }
+        /// <summary></summary>
+        void StartGameButton_Click()
+        {
+        }
+        /// <summary></summary>
+        void LoginOutButton_Click()
+        {
+        }
+
+
+
+        /// <summary>释放UI引用</summary>
+        public override void Dispose()
+        {
+        }
+    }
+}
+
---
+++ HotFix/Module/UI/UIPanel/View/LoginUIView.cs
@@ -0,0 +1,41 @@
+﻿//工具生成不要修改
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace HotFix
+{
+    public partial class LoginUI : BaseUI
+    {
+        private GameObject PasswordLogin;
+        private InputField InputFieldUserName;
+        private InputField InputFieldPassword;
+        private Button LoginButton;
+        private Button SdkLogin;
+        private GameObject GoSelServer;
+        private Text CurServerName;
+        private Button ChangServerBtn;
+        private Button StartGameButton;
+        private Text LoginUserName;
+        private Button LoginOutButton;
+ 
+        /// <summary>初始化UI控件</summary>
+        public override void InitComponent()
+        {
+            PasswordLogin = Get("PasswordLogin");
+            InputFieldUserName = Get<InputField>("InputFieldUserName");
+            InputFieldPassword = Get<InputField>("InputFieldPassword");
+            LoginButton = Get<Button>("LoginButton");
+            SdkLogin = Get<Button>("SdkLogin");
+            GoSelServer = Get("GoSelServer");
+            CurServerName = Get<Text>("CurServerName");
+            ChangServerBtn = Get<Button>("ChangServerBtn");
+            StartGameButton = Get<Button>("StartGameButton");
+            LoginUserName = Get<Text>("LoginUserName");
+            LoginOutButton = Get<Button>("LoginOutButton");
+
+        }
+    }
+}
+
