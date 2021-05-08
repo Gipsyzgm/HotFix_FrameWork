8158592c57808217d43b626e40551ba7c97e7da2
Gipsyzgm <458586540@qq.com>
2021-05-08 11:12:07 +0800

    微调

--- HotFix/Module/UI/Extend/Confirm.cs
+++ HotFix/Module/UI/Extend/Confirm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -11,9 +11,13 @@ namespace HotFix.Module.UI
     public class Confirm
     {
         /// <summary>
-        /// 缓存ConfirmItem实例，留着下次使用
+        /// 缓存ConfirmItem实例，留着下次使用，关闭的都移进来
         /// </summary>
         public static Queue<ConfirmItem> CacheConfirmsList = new Queue<ConfirmItem>();
+        /// <summary>
+        /// 需要手动关闭的ConfirmItem实例。
+        /// </summary>
+        public static Stack<ConfirmItem> ConfirmNeedCloseList = new Stack<ConfirmItem>();
 
         /// <summary>
         /// 显示Confirm
@@ -22,11 +26,25 @@ namespace HotFix.Module.UI
         /// <param name="cancelCB">取消回调</param>
         /// <param name="content">消息内容</param>
         /// <param name="title">标题,不传使用默认标题</param>
+        /// <param name="autoClose">是否点击按钮后自动关闭页面，不需要的话需要手动调用ManualClose()关闭</param>
         /// <param name="ShowMask">是否使用遮罩</param>
-        public static void Show(Action confirmCB, Action cancelCB, string content, string title = null, bool ShowMask = true) 
+        public static void Show(Action confirmCB, Action cancelCB, string content, string title = null, bool autoClose = true, bool ShowMask = true) 
         {
-            RunShow(confirmCB, cancelCB, content,title, ShowMask).Run();
+            RunShow(confirmCB, cancelCB, content, title, autoClose, ShowMask).Run();
         }
+
+        /// <summary>
+        /// 确认框
+        /// </summary>
+        /// <param name="confirmCB">确认回调</param>
+        /// <param name="content">消息内容</param>
+        /// <param name="title">标题,不传使用默认标题</param>
+        /// <param name="ShowMask">是否使用遮罩</param>
+        public static void ShowConfirm(Action confirmCB, string content, string title = null, bool autoClose = true, bool ShowMask = true)
+        {      
+            RunShow(confirmCB, null, content, title, autoClose, ShowMask).Run();
+        }
+
         /// <summary>
         /// 显示确认框
         /// </summary>
@@ -34,82 +52,37 @@ namespace HotFix.Module.UI
         /// <param name="cancelCB">取消回调</param>
         /// <param name="content">消息内容</param>
         /// <param name="title">标题,不传使用默认标题</param>
-        private static async CTask RunShow(Action confirmCB,Action cancelCB,string content,string title,bool ShowMask)
+        /// <param name="ShowMask">是否使用遮罩</param>
+        private static async CTask RunShow(Action confirmCB,Action cancelCB,string content,string title,bool autoClose, bool ShowMask)
         {
-            //title = title ?? Mgr.Lang.Get("Com_ConfirmTitle");
             ConfirmItem item;
             if (CacheConfirmsList.Count > 0)
             {
-                item = CacheConfirmsList.Dequeue();
-                item.SetMask(ShowMask);
-                item.ShowCustomConfirm(content, title, confirmCB, cancelCB);
+                item = CacheConfirmsList.Dequeue();      
             }
             else
             {
                 item = new ConfirmItem();
-                await item.Instantiate(HotMgr.UI.layer_dict[PanelLayer.UITips]);
-                item.SetMask(ShowMask);
-                item.ShowCustomConfirm(content, title, confirmCB, cancelCB);
-            }                
+                await item.Instantiate(HotMgr.UI.layer_dict[PanelLayer.UITips]);             
+            }
+            item.SetMask(ShowMask);
+            item.SetAutoClose(autoClose);
+            item.ShowCustomConfirm(content, title, confirmCB, cancelCB);
+            //把需要手动关闭的Item放进list
+            if (autoClose == false)
+            {
+                ConfirmNeedCloseList.Push(item);
+            }
+            
         }
-
-    //    /// <summary>
-    //    /// 显示确认框
-    //    /// </summary>
-    //    /// <param name="confirmCB">确认回调</param>
-    //    /// <param name="cancelCB">取消回调</param>
-    //    /// <param name="content">消息内容(语言Key)</param>
-    //    /// <param name="title">标题,不传使用默认标题(语言Key)</param>
-    //    public static void ShowLang(Action confirmCB, Action cancelCB, string content, string title = null,bool IsConfirmCB = false)
-    //    {
-    //        title = title == null ? Mgr.Lang.Get("Com_ConfirmTitle") : Mgr.Lang.Get(title);
-    //        Mgr.UI.Show<Confirm>(confirmCB, cancelCB, Mgr.Lang.Get(content), title, false, IsConfirmCB);
-    //    }
-
-    //    /// <summary>
-    //    /// 警告框（只有一个确定按钮）
-    //    /// </summary>
-    //    /// <param name="confirmCB">确认回调</param>
-    //    /// <param name="content">消息内容</param>
-    //    /// <param name="title">标题,不传使用默认标题</param>
-    //    public static void Alert(Action confirmCB, string content, string title = null)
-    //    {
-    //        title = title ?? Mgr.Lang.Get("Com_AlertTitle");
-    //        Mgr.UI.Show<Confirm>(confirmCB, null,content, title,true,false);
-    //    }
-
-    //    /// <summary>
-    //    /// 警告框（只有一个确定按钮）
-    //    /// </summary>
-    //    /// <param name="confirmCB">确认回调</param>
-    //    /// <param name="content">消息内容(语言Key)</param>
-    //    /// <param name="title">标题,不传使用默认标题(语言Key)</param>
-    //    public static void AlertLang(Action confirmCB, string content, string title = null)
-    //    {
-    //        title = title==null?Mgr.Lang.Get("Com_AlertTitle"): Mgr.Lang.Get(title);
-    //        Mgr.UI.Show<Confirm>(confirmCB, null, Mgr.Lang.Get(content), title, true,false);
-    //    }
-
-    //    /// <summary>
-    //    /// 断线提示 层级要调到最上面
-    //    /// </summary>
-    //    /// <param name="confirmCB">确认回调</param>
-    //    /// <param name="content">消息内容(语言Key)</param>
-    //    /// <param name="title">标题,不传使用默认标题(语言Key)</param>
-    //    public static async CTask AlertLangTop(Action confirmCB, string content, string title = null)
-    //    {
-    //        title = title == null ? Mgr.Lang.Get("Com_AlertTitle") : Mgr.Lang.Get(title);
-    //        Confirm confirm  = Mgr.UI.Show<Confirm>(confirmCB, null, Mgr.Lang.Get(content), title, true,false);
-    //        await confirm.Await();
-    //        Canvas can = confirm.gameObject.GetComponent<Canvas>();
-    //        if (can == null)
-    //        {
-    //            can = confirm.gameObject.AddComponent<Canvas>();
-    //            //can.overrideSorting = true;
-    //            //can.sortingOrder = 60;
-    //            confirm.gameObject.AddComponent<GraphicRaycaster>();
-    //        }
-    //    }
+        //手动关闭最后一个需要关闭的Confirm
+        public static void ManualClose() 
+        {
+            if (ConfirmNeedCloseList.Count>0)
+            {
+                ConfirmNeedCloseList.Pop().CloseSelf();
+            }
+        }
     }
 }
 
