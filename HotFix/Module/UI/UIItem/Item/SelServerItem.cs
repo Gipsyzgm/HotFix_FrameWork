dac455e73202a50684efcaeb4a6b66cce7971618
Gipsyzgm <458586540@qq.com>
2021-05-08 14:33:21 +0800

    微调

--- HotFix/Module/UI/UIItem/Item/SelServerItem.cs
+++ HotFix/Module/UI/UIItem/Item/SelServerItem.cs
@@ -1,3 +1,4 @@
+using HotFix.Module.Login;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,8 @@ namespace HotFix
     public partial class SelServerItem : BaseItem
     {
 
+        public Action<SelServerItem> onClick;
+        public ServerItemData Data;
         /// <summary>添加按钮事件</summary>
         public override void Init()
         {
@@ -15,21 +18,47 @@ namespace HotFix
 
         }
 
+        /// <summary>设置数据</summary><param name="data"></param>
+        public void SetData(ServerItemData data)
+        {
+            Data = data;
+            Refresh();
+        }
+
         /// <summary>刷新Item</summary>
         public override void Refresh()
         {
+            if (!IsInstance) return;
+            ServerNameText.text = Data.ServerName;
+            ServerID.text = Data.ServerId.ToString();
+            switch (Data.Flag) //服务器标记
+            {
+                case 1: //新服 暂时只一个标记
+                    FlagImage.SetSprite("Icon_New", UIAtlas.Common).Run();
+                    FlagImage.gameObject.SetActive(true);
+                    break;
+                default:
+                    FlagImage.gameObject.SetActive(false);
+                    break;
+            }
+            if (Data.URL == ServerListMgr.I.ServerURL)
+            {
+                FlagImage.SetSprite("Icon_Last", UIAtlas.Common).Run();
+                FlagImage.gameObject.SetActive(true);
+            }
         }
-
         /// <summary>当前对象点击事件</summary>
         void self_Click()
         {
-
+            onClick?.Invoke(this);
         }
 
         /// <summary>释放Item引用</summary>
         public override void Dispose()
         {
-
+            base.Dispose();
+            Data = null;
+            onClick = null;
         }
     }
 }
