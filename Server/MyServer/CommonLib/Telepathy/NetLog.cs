bb4707637411aca3548381cfe4b8eb37216d3e41
Gipsyzgm <458586540@qq.com>
2021-05-18 14:30:25 +0800

    微调

--- Server/MyServer/CommonLib/Telepathy/NetLog.cs
+++ Server/MyServer/CommonLib/Telepathy/NetLog.cs
@@ -1,8 +1,9 @@
-﻿// A simple logger class that uses Console.WriteLine by default.
+// A simple logger class that uses Console.WriteLine by default.
 // Can also do Logger.LogMethod = Debug.Log for Unity etc.
 // (this way we don't have to depend on UnityEngine.DLL and don't need a
 //  different version for every UnityEngine version here)
 using CommonLib;
+using Google.Protobuf;
 using System;
 
 namespace Telepathy
@@ -27,7 +28,7 @@ namespace Telepathy
     public static class NetLog
     {
         //日志输出类型
-        private static ELogType logType = ELogType.Error | ELogType.Warning;//ELogType.All;
+        private static ELogType logType = ELogType.Error | ELogType.Warning;
 
         public static void SetLogType(ELogType type)
         {
@@ -73,14 +74,14 @@ namespace Telepathy
         /// 通讯消息日志
         /// </summary>
         /// <param name="args"></param>
-        //public static void LogMsg(bool isSend, ushort protocol, int length, IMessage msg)
-        //{
-        //    if (protocol <= 100) return; //100以下的消息都过滤掉，一般为心跳包
-        //    if ((logType & ELogType.Msg) == ELogType.Msg)
-        //    {
-        //        Logger.LogMsg(isSend, $"{(isSend ? "发送" : "收到")}[{protocol } L:{length} {msg.GetType().FullName}]:{msg.ToString()}");
-        //    }
-        //}
+        public static void LogMsg(bool isSend, ushort protocol, int length, IMessage msg)
+        {
+            if (protocol <= 100) return; //100以下的消息都过滤掉，一般为心跳包
+            if ((logType & ELogType.Msg) == ELogType.Msg)
+            {
+                Logger.LogMsg(isSend, $"{(isSend ? "发送" : "收到")}[{protocol } L:{length} {msg.GetType().FullName}]:{msg.ToString()}");
+            }
+        }
     }
 
 }
