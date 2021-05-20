0eb883d0b251d08fdcd1fc58b22efc6f485a30fa
Gipsyzgm <458586540@qq.com>
2021-05-20 12:01:58 +0800

    微调

--- HotFix/Net/NetMgr.cs
+++ HotFix/Net/NetMgr.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -6,6 +6,9 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using UnityEngine;
 using Telepathy;
+using Google.Protobuf;
+using HotFix.Net;
+using CSocket;
 
 namespace HotFix
 {
@@ -40,67 +43,64 @@ namespace HotFix
         /// <summary>
         /// 发送消息
         /// </summary>        
-        //public void Send<T>(T data) where T : IMessage
-        //{
-        //    if (!IsConnect)
-        //    {
-        //        Debug.LogError("服务器未连接");
-        //        //if (! Mgr.UI.IsOpen<Confirm>())
-        //        //    showDisconnectConfirm();
-
-        //        return;
-        //    }
-        //    ushort protocol = ClientToGameClientProtocol.Instance.GetProtocolByType(data.GetType());
-        //    if (protocol == 0)
-        //    {
-        //        Debug.LogError("ProtocolType 协议号未定义,协议类型:"+data.GetType());
-        //        return;
-        //    }
-        //    if (!MsgWaiting.Show(protocol)) //显示并判断可发送
-        //        return;
-        //    byte[] body = data.ToByteArray();
-        //    LogMsg(true, protocol, body.Length, data);
-
-        //    byte[] md5 = null;
-        //    if (ClientToGameClientProtocol.Instance.IsEncryptProtocol(protocol))
-        //        md5 = CSocketUtils.MD5Encrypt(body, protocol);
-
-        //    byte[] package;
-        //    if (md5 != null)
-        //        package = new byte[body.Length + 2 + md5.Length];
-        //    else
-        //        package = new byte[body.Length + 2];
-        //    Array.Copy(BitConverter.GetBytes(protocol), 0, package, 0, 2);  //协议号
-        //    Array.Copy(body, 0, package, 2, body.Length);
-        //    if (md5 != null)
-        //        Array.Copy(md5, 0, package, 2 + body.Length, md5.Length);
-        //    _socket.Send(new ArraySegment<byte>(package));
-        //}
-
-        public void Send(byte[] data) 
+        public void Send<T>(T data) where T : IMessage
         {
             if (!IsConnect)
             {
                 Debug.LogError("服务器未连接");
                 //if (! Mgr.UI.IsOpen<Confirm>())
                 //    showDisconnectConfirm();
-
                 return;
             }
-            byte[] body = data;      
-            byte[] package = data;
-          
-            //package = new byte[body.Length + 2];
-            //Array.Copy(BitConverter.GetBytes(protocol), 0, package, 0, 2);  //协议号
-            //Array.Copy(body, 0, package, 2, body.Length);
+            ushort protocol = ClientToGameClientProtocol.Instance.GetProtocolByType(data.GetType());
+            if (protocol == 0)
+            {
+                Debug.LogError("ProtocolType 协议号未定义,协议类型:" + data.GetType());
+                return;
+            }
+            if (!MsgWaiting.Show(protocol)) //显示并判断可发送
+                return;
+            byte[] body = data.ToByteArray();
+            LogMsg(true, protocol, body.Length, data);
 
+            byte[] md5 = null;
+            if (ClientToGameClientProtocol.Instance.IsEncryptProtocol(protocol))
+                md5 = CSocketUtils.MD5Encrypt(body, protocol);
+
+            byte[] package;
+            if (md5 != null)
+                package = new byte[body.Length + 2 + md5.Length];
+            else
+                package = new byte[body.Length + 2];
+            Array.Copy(BitConverter.GetBytes(protocol), 0, package, 0, 2);  //协议号
+            Array.Copy(body, 0, package, 2, body.Length);
+            if (md5 != null)
+                Array.Copy(md5, 0, package, 2 + body.Length, md5.Length);
             _socket.Send(new ArraySegment<byte>(package));
+        }
+
+        /// <summary>
+        /// 通讯消息日志
+        /// </summary>
+        /// <param name="args"></param>
+        private void LogMsg(bool isSend, ushort protocol, int length, IMessage msg)
+        {
+            if (protocol < 10) return;
+            Debug.Log($"{(isSend ? "发送" : "收到")}[{protocol } L:{length} {msg.GetType().FullName}]:{Dumper.DumpAsString(msg)}");
+        }
+        public void TestSend(byte[] data) 
+        {
+            if (!IsConnect)
+            {
+                Debug.LogError("服务器未连接");              
+                return;
+            }       
+            _socket.Send(new ArraySegment<byte>(data));
         }
         public void Update()
         {
             if (_socket.ReceivePipeCount> 0)
             {
-                Debug.LogError("执行了吗？");
                 _socket.Tick(1);              
             }
         }
@@ -116,9 +116,12 @@ namespace HotFix
         /// <param name="buff"></param>
         public void OnData(ArraySegment<byte> buff)
         {
+            //收到消息解析一下，获取真实长度的数据
+            byte[] bytes = new byte[buff.Count];
+            Buffer.BlockCopy(buff.Array, buff.Offset, bytes, 0, buff.Count);
 
-            Debug.LogError("收到回复信息");
-            Debug.LogError(System.Text.Encoding.Default.GetString(buff.Array)+":111");
+            Debug.LogError("收到回复信息，信息长度："+ bytes.Length);
+            Debug.LogError(System.Text.Encoding.Default.GetString(bytes) + ":111");
 
 
         }
@@ -221,19 +224,18 @@ namespace HotFix
         /// <summary>
         /// 主动断开连接
         /// </summary>
-        //public void Close(bool isAlertMsg = true)
-        //{
-        //    Debug.Log("主动断开Socket连接");
-        //    ReLoginMgr.IsReLogin = false;
-        //    if (IsConnect)
-        //    {
-        //        _isAlertMsg = isAlertMsg;
-        //        _socket.Disconnect();
-        //        //Dispose();
-        //        //_socket.SendString("");
-        //    }
-        //    noSendMessage.Clear();
-        //}
+        public void Close(bool isAlertMsg = true)
+        {
+            Debug.Log("主动断开Socket连接");
+            //ReLoginMgr.IsReLogin = false;
+            if (IsConnect)
+            {
+                _isAlertMsg = isAlertMsg;
+                _socket.Disconnect();
+                
+            }
+            //noSendMessage.Clear();
+        }
         //重连短开测试
         public void ReClose()
         {
