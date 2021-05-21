240fd02742c5aff8d18fc9b1e8079d70f3332582
Gipsyzgm <458586540@qq.com>
2021-05-21 12:19:14 +0800

    Version版本信息调整

--- MyPro/Assets/MyCore/Manager/VersionCheck/VersionInfo.cs
+++ MyPro/Assets/MyCore/Manager/VersionCheck/VersionInfo.cs
@@ -8,16 +8,9 @@ public class VersionInfo
     public string Platform { get; set; }
     /// <summary>客户端版本 </summary>
     public string AppVersion { get; set; }
-
     /// <summary>是否强制更新</summary>
     public bool IsForcedUpdate { get; set; }
     /// <summary>强更包路径</summary>
     public string AppDownloadURL { get; set; }
-
-    /// <summary>资源下载地址</summary>
-    public string ResURL { get; set; }
-
-    /// <summary>登陆验证地址</summary>
-    public string LoginUrl { get; set; }
 
 }
