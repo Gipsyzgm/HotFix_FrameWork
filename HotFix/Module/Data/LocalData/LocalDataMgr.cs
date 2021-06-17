c5923b5a6f3f9a4dc128e0c0d90c6970c083696f
Gipsyzgm <458586540@qq.com>
2021-06-17 18:12:40 +0800

    增加本地数据管理

--- HotFix/Module/Data/LocalData/LocalDataMgr.cs
+++ HotFix/Module/Data/LocalData/LocalDataMgr.cs
@@ -15,7 +15,7 @@ namespace HotFix
         public void LoadAll()
         {
             TPlayer player = Load<TPlayer>();
-            bool isNewPlayer = false;
+            bool IsNewPlayer = false;
             if (player == null)
             {
                 player = new TPlayer()
@@ -23,31 +23,21 @@ namespace HotFix
                     Power = 20,
                     Level = 1,
                 };
-                isNewPlayer = true;
+                IsNewPlayer = true;
                 player.Save();
             }                
-            if (isNewPlayer) //加一个新装备
+            if (IsNewPlayer) //加一个新装备
             {
                 ItemMgr.I.AddNewItem(4001, 10, EItemEquipPlace.OutWeapon, false); //加一个外刀
-                //ItemMgr.I.AddNewItem(4002, 1, EItemEquipPlace.OutWeapon, false); //加一个外刀
-                //ItemMgr.I.AddNewItem(4003, 1, EItemEquipPlace.OutWeapon, false); //加一个外刀
-                //ItemMgr.I.AddNewItem(4004, 1, EItemEquipPlace.OutWeapon, false); //加一个外刀
-                //ItemMgr.I.AddNewItem(4005, 1, EItemEquipPlace.OutWeapon, false); //加一个外刀
-
+               
                 ItemMgr.I.AddNewItem(2001, 1, EItemEquipPlace.Hero, false);  //增加一个英雄面具
-                //ItemMgr.I.AddNewItem(2002, 1, EItemEquipPlace.Hero, false);  //增加一个英雄面具
+                
 
                 ItemMgr.I.AddNewItem(5004, 1, EItemEquipPlace.InnerWeapon, false); //加一个内刀
-                //ItemMgr.I.AddNewItem(5002, 1, EItemEquipPlace.None, false); //加一个内刀
-                //ItemMgr.I.AddNewItem(5003, 1, EItemEquipPlace.None, false); //加一个内刀
-                //ItemMgr.I.AddNewItem(5004, 1, EItemEquipPlace.None, false); //加一个内刀
-
-                //ItemMgr.I.AddNewItem(6001, 1, EItemEquipPlace.None, false); //增加测试戒指A
-                //ItemMgr.I.AddNewItem(6002, 1, EItemEquipPlace.None, false); //增加测试戒指B
-                //ItemMgr.I.AddNewItem(6003, 1, EItemEquipPlace.None, false); //增加测试戒指C
+                
 
                 ItemMgr.I.AddNewItem(3001, 1, EItemEquipPlace.None, false); //增加宠物A
-                //ItemMgr.I.AddNewItem(3002, 1, EItemEquipPlace.None, false); //增加宠物B
+                
                 if (Application.isEditor)
                 {
                     ItemMgr.I.AddNewItem(1, 1000000, EItemEquipPlace.None, false); //增加金币
@@ -56,8 +46,6 @@ namespace HotFix
                 {
                     ItemMgr.I.AddNewItem(1, 1000, EItemEquipPlace.None, false); //增加金币
                 }
-
-
             }
 
             PlayerMgr.PlayerWarData.Recount();
--- HotFix/Module/Time/TimeMgr.cs
+++ HotFix/Module/Time/TimeMgr.cs
@@ -1,4 +1,5 @@
-﻿
+
+using PbSystem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,100 +8,90 @@ using System.Threading.Tasks;
 using System.Timers;
 using UnityEngine;
 
-
-public class TimeMgr
+namespace HotFix
 {
-//    /// <summary>
-//    /// 服务器时间戳
-//    /// </summary>
-//    private double _serverTimestamp = 0;
+    public class TimeMgr
+    {
+        /// <summary>
+        /// 服务器时间戳
+        /// </summary>
+        private double _serverTimestamp = 0;
+        /// <summary>
+        /// 服务器时间戳
+        /// </summary>
+        public int ServerTimestamp => (int)_serverTimestamp;
 
-//    /// <summary>
-//    /// 服务器时间戳
-//    /// </summary>
-//    public int ServerTimestamp => (int)_serverTimestamp;
+        /// <summary>
+        /// 服务器时间
+        /// </summary>
+        public DateTime ServerTime => ((int)_serverTimestamp).ToDateTime();
 
-//    /// <summary>
-//    /// 服务器时间
-//    /// </summary>
-//    public DateTime ServerTime => ((int)_serverTimestamp).ToDateTime();
+        protected int timerRunId = 0;
 
-//    //private Task _task;
-//    protected int timerRunId = 0;
+        //最后一次更新服务时间的时间戳(用本地时间,切出去服务器时间可能没有走)
+        public int LastUpdateServerTimestamp = 0;
 
-//    //最后一次更新服务时间的时间戳(用本地时间,切出去服务器时间可能没有走)
-//    public int LastUpdateServerTimestamp = 0;
+        public int ServerStartTime = 0;
+        public TimeMgr()
+        {
+            _serverTimestamp = DateTime.Now.ToTimestamp();
+        }
 
-//    public int ServerStartTime = 0;
-//    public TimeMgr()
-//    {
-//        _serverTimestamp = DateTime.Now.ToTimestamp();
-//    }
+        private float heartbeatCount = 0;
 
-//    private float heartbeatCount = 20;
+        CS_sys_heartbeat msg = new CS_sys_heartbeat();
+        private void serverTimer_Elapsed()
+        {    
+            _serverTimestamp += 0.2f;         
+            heartbeatCount += 0.2f;
+            if (heartbeatCount > 30)//30秒发送一次心跳包
+            {
+                heartbeatCount = 0;
+                HotMgr.Net.Send(msg);
+            }           
+        }
 
-//    private int lastHorse = -1;
-//    CS_sys_heartbeat msg = new CS_sys_heartbeat();
-//    private void serverTimer_Elapsed()
-//    {
-//        //float lastTime = Time.realtimeSinceStartup;         
-//        //float interval = (Time.realtimeSinceStartup - lastTime);
-//        _serverTimestamp += 0.2f;
-//        //lastTime = Time.realtimeSinceStartup;
-//        heartbeatCount += 0.2f;
-//        if (heartbeatCount > 30)//30秒发送一次心跳包
-//        {
-//            heartbeatCount = 0;
-//            Mgr.Net.Send(msg);
-//        }
-//        if ((int)_serverTimestamp / 3600 != lastHorse)
-//        {
-//            lastHorse = (int)_serverTimestamp / 3600;
-//        }
-//    }
+        /// <summary>
+        /// 获取服务器时间所在的周天(1-7)
+        /// </summary>
+        /// <returns></returns>
+        public int GetWeek()
+        {
+            DayOfWeek week = ServerTime.DayOfWeek;
+            if (week == DayOfWeek.Sunday)
+                return 7;
+            return (int)week;
+        }
 
-//    /// <summary>
-//    /// 获取服务器时间所在的周天(1-7)
-//    /// </summary>
-//    /// <returns></returns>
-//    public int GetWeek()
-//    {
-//        DayOfWeek week = ServerTime.DayOfWeek;
-//        if (week == DayOfWeek.Sunday)
-//            return 7;
-//        return (int)week;
-//    }
+        /// <summary>
+        /// 设置服务器时间
+        /// </summary>
+        /// <param name="timestamp"></param>
+        public void UpdateServerTimestamp(int timestamp)
+        {
+            _serverTimestamp = timestamp;
+            if (timerRunId == 0)
+            {
+                timerRunId = HotMgr.Timer.Loop(0.2f, serverTimer_Elapsed);
+            }
+        }
 
-//    /// <summary>
-//    /// 设置服务器时间
-//    /// </summary>
-//    /// <param name="timestamp"></param>
-//    public void UpdateServerTimestamp(int timestamp)
-//    {
-//        _serverTimestamp = timestamp;
-//        if (timerRunId == 0)
-//        {
-//            timerRunId = Mgr.Timer.Loop(0.2f, serverTimer_Elapsed);
-//        }
-//    }
+        /// <summary>
+        /// 获取当天剩余秒数
+        /// </summary>
+        /// <returns></returns>
+        public int GetDayTimeLeftSec()
+        {
+            int s = 60 - ServerTime.Second;
+            s += (59 - ServerTime.Minute) * 60;
+            s += (23 - ServerTime.Hour) * 3600;
+            return s;
+        }
 
-//    /// <summary>
-//    /// 获取当天剩余秒数
-//    /// </summary>
-//    /// <returns></returns>
-//    public int GetDayTimeLeftSec()
-//    {
-//        int s = 60 - ServerTime.Second;
-//        s += (59 - ServerTime.Minute) * 60;
-//        s += (23 - ServerTime.Hour) * 3600;
-//        return s;
-//    }
-
-//    public void Dispose()
-//    {
-//        //isRun = false;            
-//        //_task = null;
-//        heartbeatCount = 0;
-//        timerRunId = 0;
-//    }
+        public void Dispose()
+        {
+            heartbeatCount = 0;
+            timerRunId = 0;
+        }
+    }
 }
