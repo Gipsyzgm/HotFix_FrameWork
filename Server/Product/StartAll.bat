bc3276575357f6ff6fc6ac1957f2662344274712
Gipsyzgm <458586540@qq.com>
2021-06-25 15:54:42 +0800

    启动文件

--- Server/Product/StartAll.bat
+++ Server/Product/StartAll.bat
@@ -5,25 +5,25 @@ set path=%~dp0
 
 echo 当前路径path：%path%
 
-cd %path%CenterServer\
+cd %path%CenterServer\net5.0\
 echo 启动CenterServer.exe
 start CenterServer.exe
 rem 延时1秒
 C:\Windows\system32\choice.exe /t 1 /d y /n > nul
 
-cd %path%GameServer\
+cd %path%GameServer\net5.0\
 echo 启动GameServer.exe
 start GameServer.exe
 rem 延时1秒
 C:\Windows\system32\choice.exe /t 1 /d y /n > nul
 
-cd %path%GameServer\
+cd %path%GameServer\net5.0\
 echo 启动GameServer.exe
 start GameServer.exe
 rem 延时1秒
 C:\Windows\system32\choice.exe /t 1 /d y /n > nul
 
-cd %path%LoginServer\
+cd %path%LoginServer\net5.0\
 echo 启动LoginServer.exe
 start LoginServer.exe
 rem 延时1秒
