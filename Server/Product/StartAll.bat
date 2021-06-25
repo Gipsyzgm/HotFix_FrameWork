a8ec7bad74f4fb697481613d99a0e516b3e76f98
Gipsyzgm <458586540@qq.com>
2021-06-25 15:44:01 +0800

    微调

---
+++ Server/Product/StartAll.bat
@@ -0,0 +1,35 @@
+@echo off
+
+echo 开始启动游戏服务进程
+set path=%~dp0
+
+echo 当前路径path：%path%
+
+cd %path%CenterServer\
+echo 启动CenterServer.exe
+start CenterServer.exe
+rem 延时1秒
+C:\Windows\system32\choice.exe /t 1 /d y /n > nul
+
+cd %path%GameServer\
+echo 启动GameServer.exe
+start GameServer.exe
+rem 延时1秒
+C:\Windows\system32\choice.exe /t 1 /d y /n > nul
+
+cd %path%GameServer\
+echo 启动GameServer.exe
+start GameServer.exe
+rem 延时1秒
+C:\Windows\system32\choice.exe /t 1 /d y /n > nul
+
+cd %path%LoginServer\
+echo 启动LoginServer.exe
+start LoginServer.exe
+rem 延时1秒
+C:\Windows\system32\choice.exe /t 1 /d y /n > nul
+
+
+echo 已启动所有游戏服务进程
+
+rem @pause
